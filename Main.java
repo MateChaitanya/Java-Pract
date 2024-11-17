998e34a1f1ec27ee05e581573cf63e118285ca20
MateChaitanya <chaitanyamate02@gmail.com>
2024-11-18 00:59:26 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+/* 
+// ArrayList
+import java.util.*;
+public class Collection {
+    public static void main(String[] args) {
+        ArrayList<String>list = new ArrayList<>();
+        list.add("Alice");
+        list.add("Bob");
+        System.out.println(list);
+    }
+}
+*/
+
+import java.util.LinkedList;
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.remove(1); // Removes the second element
+        System.out.println(list); // Output: [1]
+    }
+}
