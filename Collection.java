4227fc202b176ebe2911b242058f67f8d82e8a4a
MateChaitanya <chaitanyamate02@gmail.com>
2024-11-17 23:16:12 +0530

    first commit

---
+++ Collection.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Collection {
+    public static void main(String[] args) {
+        ArrayList<String>list = new ArrayList<>();
+        list.add("Alice");
+        list.add("Bob");
+        System.out.println(list);
+    }
+}
