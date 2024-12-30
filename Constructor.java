ebdad9c9018a33ed605baf900f9cbbc1584b7d44
MateChaitanya <chaitanyamate02@gmail.com>
2024-12-30 16:09:34 +0530

    Initail commit

--- Constructor.java
+++ Constructor.java
@@ -1,4 +1,4 @@
-class Student{
+/*class Student{
     int id ;
      int age;
      Student(){
@@ -16,3 +16,53 @@ public class Constructor {
     Student obj = new Student();
    } 
 } 
+*/
+
+//this Keyword
+/* 
+class ABC {
+   int num;
+
+   ABC(int num) {
+       this.num = num; // Refers to the instance variable
+   }
+
+   void display() {
+       System.out.println("Number: " + num);
+   }
+}
+
+public class Constructor {
+   public static void main(String[] args) {
+       ABC obj = new ABC(25);
+       obj.display();
+   }
+}
+*/
+
+
+//This() function
+class XYZ {
+   int num;
+   String name;
+
+   XYZ() {
+       this(100, "Default"); // Calls the parameterized constructor
+   }
+
+   XYZ(int n, String nm) {
+       num = n;
+       name = nm;
+   }
+
+   void display() {
+       System.out.println("Number: " + num + ", Name: " + name);
+   }
+}
+
+public class Constructor {
+   public static void main(String[] args) {
+       XYZ obj = new XYZ(); // Default constructor calls parameterized constructor
+       obj.display();
+   }
+}
