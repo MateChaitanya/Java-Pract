541940de74b7c0d376c0651b05764b321fa4b0bc
MateChaitanya <chaitanyamate02@gmail.com>
2025-03-02 11:12:13 +0530

    Initail commit

---
+++ Collection/ArrayListExample.java
@@ -0,0 +1,11 @@
+package Collection;
+
+public class ArrayList{
+ public static void main(String[] args) {
+    ArrayList a = new ArrayList();
+    a.add("C");
+    a.add(10);
+    a.add('A');
+    System.out.println(a);
+ }
+}
